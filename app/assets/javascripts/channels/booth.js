0a4d1f7ff83f4698f49281db95f39eb7c6c0e131
murano-masaki <murano-masaki@kayac.com>
2016-07-18 22:47:09 +0900

    complete animation.

--- app/assets/javascripts/channels/booth.js
+++ app/assets/javascripts/channels/booth.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+  // $('#booth-1').style.webkitAnimationPlayState="paused";
+};
+
 App.booth = App.cable.subscriptions.create("BoothChannel", {
   connected: function() {
     console.log("BoothChannel connected.");
@@ -11,7 +15,10 @@ App.booth = App.cable.subscriptions.create("BoothChannel", {
     console.log('received via channel.');
     console.dir(data);
     alert("トイレ" + data['boothId'] + " が空きましたよ！");
+    // TODO: html が正しくリロードできない。入れ子になる。
     $('#booth-'+data['boothId']).html(data['message']);
+    // $('#booth-'+data['boothId']).outerHTML(data['message']); // outerHTML is not trigger animation.
+    $('#booth-'+data['boothId']).addClass('booth-state-animation');
   },
 
   traffic: function(msg) {
