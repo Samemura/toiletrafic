b5e9515ba2f9b3329e353c3f00da3d64437ea0ad
murano-masaki <murano-masaki@kayac.com>
2016-10-13 14:15:01 +0900

    update BoothUsage for average and format.

--- app/models/booth_usage.rb
+++ app/models/booth_usage.rb
@@ -14,15 +14,24 @@
 #
 
 class BoothUsage < ApplicationRecord
+  EXIPRE_PERIOD = 7.days.ago
+
+  scope :created_at, ->(t=Time.now) { where(created_at: t) }
+
   belongs_to :booth
 
   class << self
-    def hourly_usage
-      self.group_by_hour(:created_at).sum(:use_minute).val_to_percent(Booth.count*60)
+    def by_hour(day=Date.today, hour_range: 8..19)
+      self.created_at(day.to_time.all_day).group_by_hour_of_day(:created_at).sum(:use_minute).select_by_key_range(hour_range).value_to_percent(Booth.count*60)
+    end
+
+    def average_by_hour(period=1.month.ago..Time.now, hour_range: 8..19)
+      days = ((period.last-period.first)/60/60/24).round
+      self.created_at(period).group_by_hour_of_day(:created_at).sum(:use_minute).select_by_key_range(hour_range).value_to_percent(Booth.count*60*days)
     end
 
     def delete_olds
-      self.where("created_at < ?", 7.days.ago).delete_all
+      self.where("created_at < ?", EXIPRE_PERIOD).delete_all
     end
   end
 end
--- config/initializers/hash.rb
+++ config/initializers/hash.rb
@@ -1,7 +1,10 @@
 class Hash
-  def val_to_percent(max)
+  def value_to_percent(max)
     self.map {|k, v|
       [k, (v.to_f*100/max).round]
     }.to_h
+  end
+  def select_by_key_range(range)
+    self.select{|k,_| range.include?(k)}
   end
 end
