4778bc9006ac00cafa534c4992e98f7b527235cd
murano-masaki <murano-masaki@kayac.com>
2016-10-13 17:32:51 +0900

    add toilets api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'booths#index'
   resources :booths, only: [:index, :show, :update], path: 'api/v1/booths', format: 'json'
+  resources :toilets, only: [:index, :show], path: 'api/v1/toilets', format: 'json'
 
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
