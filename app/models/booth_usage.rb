c12ee87997f889bd0c1dd9d50cb8bd04c714f86e
murano-masaki <murano-masaki@kayac.com>
2016-10-18 13:58:39 +0900

    fix average bug.

--- app/models/booth_usage.rb
+++ app/models/booth_usage.rb
@@ -14,7 +14,7 @@
 #
 
 class BoothUsage < ApplicationRecord
-  EXIPRE_PERIOD = 8.days.ago.at_end_of_day
+  EXPIRE_PERIOD = 8.days.ago.at_end_of_day
 
   scope :created_at, ->(t=Time.now) { where(created_at: t) }
   scope :without_day_of_week, ->(day) { where("DAYOFWEEK(created_at) != ?", day)}
@@ -26,17 +26,17 @@ class BoothUsage < ApplicationRecord
       self.created_at(day.to_time.all_day).group_by_hour_of_day(:created_at).sum(:use_minute).select_by_key_range(hour_range).value_to_percent(Booth.count*60)
     end
 
-    def average_by_hour(period=EXIPRE_PERIOD..1.day.ago.at_end_of_day, hour_range: 8..19)
-      days = ((period.last-period.first).to_f/60/60/24) - self.weekdays(period)
+    def average_by_hour(period=EXPIRE_PERIOD..1.day.ago.at_end_of_day, hour_range: 8..19)
+      days = ((period.last-period.first).to_f/60/60/24) - self.weekends(period)
       self.created_at(period).without_day_of_week(1).without_day_of_week(7).group_by_hour_of_day(:created_at).sum(:use_minute).select_by_key_range(hour_range).value_to_percent(Booth.count*60*days)
     end
 
     def delete_olds
-      self.where("created_at < ?", EXIPRE_PERIOD).delete_all
+      self.where("created_at < ?", EXPIRE_PERIOD).delete_all
     end
 
-    def weekdays(period)
-      period.first.to_datetime.step(period.last.to_datetime, 1).inject(0){|sum, p| sum + ([0,6].exclude?(p.wday) ? 1 : 0)}
+    def weekends(period)
+      period.first.to_datetime.step(period.last.to_datetime, 1).inject(0){|sum, p| sum + ([0,6].include?(p.wday) ? 1 : 0)}
     end
   end
 end
